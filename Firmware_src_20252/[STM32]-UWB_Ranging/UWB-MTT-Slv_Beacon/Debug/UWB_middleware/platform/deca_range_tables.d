401673ccaf61bbdb457646b6ec79b28a6ef3dfd3
nhathaivu <hainhatquangvu@gmail.com>
2026-03-10 23:16:28 +0700

    add .gitignore

--- Firmware_src_20252/[STM32]-UWB_Ranging/UWB-MTT-Slv_Beacon/Debug/UWB_middleware/platform/deca_range_tables.d
+++
@@ -1,8 +0,0 @@
-UWB_middleware/platform/deca_range_tables.o: \
- ../UWB_middleware/platform/deca_range_tables.c \
- F:/STM32CubeIDE_1.19.0/UWB-MTT-Slv_Beacon/UWB_middleware/decadriver/deca_device_api.h \
- F:/STM32CubeIDE_1.19.0/UWB-MTT-Slv_Beacon/UWB_middleware/decadriver/deca_param_types.h \
- F:/STM32CubeIDE_1.19.0/UWB-MTT-Slv_Beacon/UWB_middleware/decadriver/deca_types.h
-F:/STM32CubeIDE_1.19.0/UWB-MTT-Slv_Beacon/UWB_middleware/decadriver/deca_device_api.h:
-F:/STM32CubeIDE_1.19.0/UWB-MTT-Slv_Beacon/UWB_middleware/decadriver/deca_param_types.h:
-F:/STM32CubeIDE_1.19.0/UWB-MTT-Slv_Beacon/UWB_middleware/decadriver/deca_types.h:
